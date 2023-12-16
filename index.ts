ec4bd9f3eb5178c038518596b8be50df80a5eeb4
najim3 <engr.najim3@gmail.com>
2023-12-16 18:02:14 +0600

    One Quick change

---
+++ index.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1"
+
+axios.get(url).then(response => {
+  const todo = response.data;
+
+  const ID = todo.ID
+  const title = todo.title
+  const finished = todo.finished
+
+  console.log(`
+    The Todo with id: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
+  
+}
+)
